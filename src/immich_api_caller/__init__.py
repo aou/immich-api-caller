ccff9e364f0253fcca81eeedc60f79f8c7ea790c
Alexander Ou <alexou@gmail.com>
2025-04-08 09:54:09 -0700

    request dummy code

--- src/immich_api_caller/__init__.py
+++ src/immich_api_caller/__init__.py
@@ -5,3 +5,15 @@ config = dotenv_values()
 
 if "IMMICH_API_KEY" not in config.keys():
     raise ValueError("IMMICH_API_KEY not defined in environment")
+
+
+baseurl = "http://localhost:2283"
+
+endpoint = "/api/albums"
+
+url = baseurl + endpoint
+
+payload = {}
+headers = {"Accept": "application/json", "x-api-key": config["IMMICH_API_KEY"]}
+
+response = requests.request("GET", url, headers=headers, data=payload)
